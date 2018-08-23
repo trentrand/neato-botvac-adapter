1bdcc8d9e13b6c64bcf819e7e0d7bece0d714b60
Trent Rand <trand@squarespace.com>
2018-08-23 13:45:19 -0400

    Authenticate with Neato API on adapter instantiation

--- neato-botvac-adapter.js
+++ neato-botvac-adapter.js
@@ -1,11 +1,33 @@
 'use strict';
 
+const Neato = require('node-botvac');
 const {Adapter, Device, Property} = require('gateway-addon');
 
 class NeatoAdapter extends Adapter {
   constructor(addonManagerInstance, manifest, _errorCallback) {
     super(addonManagerInstance, 'NeatoBotvacConnectedAdapter', manifest.name);
     addonManagerInstance.addAdapter(this);
+
+    const {email, password} = manifest.moziot.config.credentials;
+    const forceLogin = false; // force login if already authorized
+
+    this.neatoClient = new Neato.Client();
+    this.neatoClient.authorize(email, password, forceLogin, (error) => {
+      if (error) {
+        console.log(error);
+        console.log('Make sure you\'ve entered valid credentials on ' +
+          'the adapter\'s configuration panel');
+
+        _errorCallback(manifest.id, error);
+      }
+
+      console.log('Neato adapter login was successful!');
+      this.startPairing();
+    });
+  }
+
+  startPairing() {
+
   }
 }
 
